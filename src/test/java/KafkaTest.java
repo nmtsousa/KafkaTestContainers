6980db15d9b33e160e5777478d1278ad7a737e8b
Nuno Sousa <nuno.mt.sousa@gmail.com>
2023-11-13 16:07:19 +0000

    Part I of the article series.

---
+++ src/test/java/KafkaTest.java
@@ -0,0 +1,51 @@
+import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.streams.StreamsConfig;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.Properties;
+
+@Testcontainers
+class KafkaTest {
+
+    private static final Network NETWORK = Network.newNetwork();
+
+    @Container
+    private static final KafkaContainer KAFKA_CONTAINER = new KafkaContainer(
+            DockerImageName.parse("confluentinc/cp-kafka:7.5.2"))
+            .withNetwork(NETWORK);
+
+    @Container
+    private static final GenericContainer<?> SCHEMA_REGISTRY = new GenericContainer<>(
+            DockerImageName.parse("confluentinc/cp-schema-registry:7.5.2"))
+            .withNetwork(NETWORK)
+            .withExposedPorts(8081)
+            .withEnv("SCHEMA_REGISTRY_HOST_NAME", "schema-registry")
+            .withEnv("SCHEMA_REGISTRY_LISTENERS", "http://0.0.0.0:8081")
+            .withEnv("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS",
+                    "PLAINTEXT://" + KAFKA_CONTAINER.getNetworkAliases().get(0) + ":9092")
+            .waitingFor(Wait.forHttp("/subjects").forStatusCode(200));
+
+    @Test
+    void testListTopics() {
+
+        Properties kafkaSettings = new Properties();
+        kafkaSettings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
+                KAFKA_CONTAINER.getBootstrapServers());
+        kafkaSettings.put("schema.registry.url",
+                "http://" + SCHEMA_REGISTRY.getHost() +
+                        ":" + SCHEMA_REGISTRY.getFirstMappedPort());
+
+        // Remaining KAFKA Settings
+
+        try (Admin admin = Admin.create(kafkaSettings)) {
+            admin.listTopics();
+        }
+    }
+}
